152850be477f17eeb4077bdfb80b6e8fbafb1e3f
thisthat587 <thisthat587@gmail.com>
2024-01-06 11:07:21 +0530

    .

--- BInary-Search/P1.java
+++ BInary-Search/P1.java
@@ -5,32 +5,31 @@ public class P1 {
 
     private void find(int num) {
         len = (arr.length - 1) / 2;
-        if(num==arr[len]){
-            isFound=true;
-        }
-        else if (num > arr[len]) {
-            for (int i = len+1; i < arr.length; i++) {
+        if (num == arr[len]) {
+            isFound = true;
+        } else if (num > arr[len]) {
+            for (int i = len + 1; i < arr.length; i++) {
                 if (arr[i] == num) {
                     isFound = true;
                 }
             }
         } else if (num < arr[len]) {
-            for (int i = len-1; i >= 0; i--) {
+            for (int i = len - 1; i >= 0; i--) {
                 if (arr[i] == num) {
                     isFound = true;
                 }
             }
         }
     }
-    
+
     public static void main(String[] args) {
         P1 obj = new P1();
         obj.find(7);
-        if(obj.isFound==true){
+        if (obj.isFound == true) {
             System.out.println("Found");
         } else {
             System.out.println("Not Found");
-            
+
         }
     }
 }
