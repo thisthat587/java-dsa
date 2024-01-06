0489e5f732aceb8c1ff4873f60f6caf01d442464
thisthat587 <thisthat587@gmail.com>
2024-01-06 16:07:40 +0530

    .

--- BInary-Search/P1.java
+++ BInary-Search/P1.java
@@ -2,27 +2,17 @@ public class P1 {
     private int arr[] = { 2, 5, 7, 8, 22, 77, 88 };
     int start = 0;
     int end = arr.length - 1;
-    int mid = 0;
 
     private boolean find(int num) {
         boolean isFound = false;
-        while (true) {
-            if (start >= arr.length) {
-                break;
-            }
-            if (end < 0) {
-                break;
-            }
-            mid = (start + end) / 2;
-            if (arr[mid] == num) {
-                isFound = true;
-                break;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] < num) {
+                end = mid - 1;
             } else if (num > arr[mid]) {
                 start = mid + 1;
-                end = arr.length - 1;
-            } else if (num < arr[mid]) {
-                start = 0;
-                end = mid - 1;
+            } else {
+                isFound = true; 
             }
         }
         return isFound;
@@ -30,7 +20,7 @@ public class P1 {
 
     public static void main(String[] args) {
         P1 obj = new P1();
-        boolean isFound = obj.find(4);
+        boolean isFound = obj.find(76);
         if (isFound == true) {
             System.out.println("Found");
         } else {
