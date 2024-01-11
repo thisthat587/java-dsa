a1a263e91382504a6612cd4ac279a1bbb32c1246
thisthat587 <thisthat587@gmail.com>
2024-01-11 22:38:38 +0530

    .

--- Binary-Search/FirstAndLastPos.java
+++ Binary-Search/FirstAndLastPos.java
@@ -5,41 +5,52 @@
 // You must write an algorithm with O(log n) runtime complexity.
 
 public class FirstAndLastPos {
+    public static void main(String[] args) {
+        int[] nums = { 7, 7, 7, 7, 7, 7, 7 };
+        int arr[];
+        int target = 7;
+        arr = searchRange(nums, target);
+        for (int n : arr) {
+            System.out.print(n + "  ");
+        }
+    }
 
     public static int[] searchRange(int[] nums, int target) {
         int arr[] = { -1, -1 };
         int start = 0;
         int end = nums.length - 1;
         int mid;
-        int flag=0;
         while (start <= end) {
             mid = (start + end) / 2;
+
             if (target == nums[mid]) {
-                flag++;
-                if(flag==1){
-                    arr[0] = mid;
-                }
-                if(flag>1){
-                    arr[1] = mid;
+                if (nums.length == 1) {
+                    arr[0] = 0;
+                    arr[1] = 0;
+                    break;
+                } else {
+                    for (int i = mid; i < nums.length && nums[i] == target; i++) {
+                        arr[1] = i;
+                    }
+                    for (int i = mid; i >= 0 && nums[i] == target; i--) {
+                        arr[0] = i;
+                    }
                 }
             }
+
             if (target > nums[mid]) {
                 start = mid + 1;
             } else {
                 end = mid - 1;
             }
         }
-        return arr;
-    }
+        if (arr[0] > arr[1]) {
 
-    public static void main(String[] args) {
-        int[] nums = { 7, 7, 7, 7, 7, 7 };
-        int arr[];
-        int target = 7;
-        arr = searchRange(nums, target);
+            arr[0] = arr[0] + arr[1];
+            arr[1] = arr[0] - arr[1];
+            arr[0] = arr[0] - arr[1];
 
-        for (int n : arr) {
-            System.out.print(n + ",");
         }
+        return arr;
     }
 }
