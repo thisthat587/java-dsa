3147d9eefab1c8f2bff19df4fa59ad09053b6833
thisthat587 <thisthat587@gmail.com>
2024-01-12 20:53:30 +0530

    .

--- Binary-Search/FindInMountainArray.java
+++ Binary-Search/FindInMountainArray.java
@@ -1,13 +1,31 @@
+// to find the minimum index of occurence of target element....
+
 public class FindInMountainArray {
-    public static void main(String[] args){
-        int arr[]={1,3,5,6,9,7,6,4,3,1};
-        int target=3;
-        findInMountain(arr,target);
+    public static void main(String[] args) {
+        int arr[] = { 1, 3, 5, 6, 9, 7, 6, 4, 3, 1 };
+        int target = 3;
+        int result = findInMountain(arr, target);
+        if (result == -1) {
+            System.out.println("Not Found at.....");
+        } else {
+            System.out.println("Found at : " + result);
+        }
+    }
 
-    }   
+    public static int findInMountain(int arr[], int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        int mid;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (arr[mid] < target) {
+                end = mid - 1;
+            } else if (arr[mid] > target) {
+                
+            } else {
 
-    public static int findInMountain(int arr[],int target){
-        
-        return 0;
+            }
+        }
+        return -1;
     }
 }
