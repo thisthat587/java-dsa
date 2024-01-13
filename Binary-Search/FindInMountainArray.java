a09590fe2d68e40af99c2dd42a0011e3e60243da
thisthat587 <thisthat587@gmail.com>
2024-01-13 11:56:56 +0530

    ...

--- Binary-Search/FindInMountainArray.java
+++ Binary-Search/FindInMountainArray.java
@@ -2,28 +2,58 @@
 
 public class FindInMountainArray {
     public static void main(String[] args) {
-        int arr[] = { 1, 3, 5, 6, 9, 7, 6, 4, 3, 1 };
+        int arr[] = { 3, 5, 6, 9, 7, 6, 4, 3, 1 };
         int target = 3;
-        int result = findInMountain(arr, target);
-        if (result == -1) {
-            System.out.println("Not Found at.....");
+        // findInMountain(arr, target);
+        if (findPeak(arr, target) == -1) {
+            System.out.println("Not Found...");
         } else {
-            System.out.println("Found at : " + result);
+            System.out.println("Found at : " + findPeak(arr, target));
         }
     }
 
-    public static int findInMountain(int arr[], int target) {
+    public static int findPeak(int arr[], int target) {
         int start = 0;
         int end = arr.length - 1;
-        int mid;
-        while (start <= end) {
+        int mid = 0;
+        while (start < end) {
             mid = start + (end - start) / 2;
-            if (arr[mid] < target) {
-                end = mid - 1;
-            } else if (arr[mid] > target) {
-                
+            if (arr[mid] > arr[mid + 1]) {
+                end = mid;
             } else {
+                start = mid + 1;
+            }
+        }
+        int result = findInAscPart(arr, target, 0, end);
+        if (result == -1) {
+            result = findInDescPart(arr, target, start, arr.length - 1);
+        }
+        return result;
+    }
 
+    public static int findInAscPart(int arr[], int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int findInDescPart(int arr[], int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
         return -1;
