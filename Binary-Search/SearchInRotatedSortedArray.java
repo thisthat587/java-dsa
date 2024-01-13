a6d39465918abe187d96d0d6bc9c3c786dc052fa
thisthat587 <thisthat587@gmail.com>
2024-01-13 18:09:17 +0530

    ...

--- Binary-Search/SearchInRotatedSortedArray.java
+++ Binary-Search/SearchInRotatedSortedArray.java
@@ -1,12 +1,30 @@
 public class SearchInRotatedSortedArray {
     public static void main(String[] args) {
-        int[] arr = { 4, 5, 6, 7, 0, 1, 2 };
+        int[] arr = { 6, 7, 0, 1, 2, 4, 5 };
+        // 6, 7, 0, 1, 2, 4, 5
         int target = 0;
-        search(arr, target);
+        System.out.println(search(arr, target));
     }
 
     public static int search(int[] nums, int target) {
-        
-        return -1;
+        int start = 0;
+        int end = nums.length - 1;
+        int mid = start + (end - start) / 2;
+        while (start < end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                if (nums[mid] > nums[mid - 1]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else {
+                if (nums[mid] > nums[mid - 1]) {
+                    return mid;
+                }
+                end = mid;
+            }
+        }
+        return start;
     }
 }
