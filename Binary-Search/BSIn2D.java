58b3a603f61ba608ee8907235cd01dabd75dfb65
thisthat587 <thisthat587@gmail.com>
2024-01-16 16:50:25 +0530

    ...

--- Binary-Search/BSIn2D.java
+++ Binary-Search/BSIn2D.java
@@ -1,24 +1,25 @@
+import java.util.*;
+
 public class BSIn2D {
     public static void main(String[] args) {
         int[][] arr = { { 10, 20, 30 }, { 15, 25, 35 }, { 25, 29, 37 } };
-        int target = 35;
-        binarySearchIn2D(arr, target);
+        int target = 37;
+        System.out.println(Arrays.toString(binarySearchIn2D(arr, target)));
     }
 
-    static void binarySearchIn2D(int[][] arr, int target) {
-        int row = arr.length;
-        int col = arr[0].length;
-        for (int i = 0; i < row;) {
-            if (arr[i][col - 1] == target) {
-                System.out.println(i + "," + --col);
-                break;
-                // return arr[i][col - 1];
-            } else if (arr[i][col - 1] < target) {
-                i++;
+    static int[] binarySearchIn2D(int[][] arr, int target) {
+        int row = 0;
+        int col = arr[0].length - 1;
+
+        while (col >= 0 && row < arr.length) {
+            if (arr[row][col] == target) {
+                return new int[] { row, col };
+            } else if (arr[row][col] < target) {
+                row++;
             } else {
                 col--;
             }
         }
-        // return -1;
+        return new int[] { -1, -1 };
     }
 }
