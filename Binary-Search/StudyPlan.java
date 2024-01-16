7132912388a1f839782d8a534b5079d3dc1aa00e
thisthat587 <thisthat587@gmail.com>
2024-01-16 10:11:12 +0530

    .

--- Binary-Search/StudyPlan.java
+++ Binary-Search/StudyPlan.java
@@ -1,39 +1,54 @@
-public class StudyPlan {
-    public static void main(String[] args) {
+public class StudyPlan
+{
+    public static void main(String[] args) 
+    {
         int[] timeForChapters = { 2, 2, 3, 3, 4, 4, 1 };
         int days = 4;
-        System.out.println("So, Ayush will have to study at least " + splitTime(timeForChapters, days)
-                + " seconds in a day to complete all the chapters in " + days + " days");
+        System.out.println("So, Ayush will have to study at least " + leastTimeRequired(timeForChapters, days)+ " seconds in a day to complete all the chapters in " + days + " days.");
     }
 
-    static int splitTime(int[] arr, int days) {
+    static int leastTimeRequired(int[] arr, int days) 
+    {
         int start = 0;
         int end = 0;
         int mid = 0;
-        for (int num : arr) {
+        
+        for (int num : arr) 
+        {
             start = Math.max(start, num);
         }
 
-        for (int num : arr) {
+        for (int num : arr) 
+        {
             end += num;
         }
 
-        while (start < end) {
+        while (start < end) 
+        {
             int sumOfTime = 0;
             mid = start + (end - start) / 2;
             int count = 1;
-            for (int num : arr) {
-                if (sumOfTime + num > mid) {
+            for (int num : arr) 
+            {
+                if (sumOfTime + num > mid) 
+                {
                     count++;
                     sumOfTime = num;
-                } else {
+                } 
+
+                else 
+                {
                     sumOfTime += num;
                 }
             }
 
-            if (count <= days) {
+            if (count <= days) 
+            {
                 end = mid;
-            } else {
+            } 
+
+            else 
+            {
                 start = mid + 1;
             }
         }
