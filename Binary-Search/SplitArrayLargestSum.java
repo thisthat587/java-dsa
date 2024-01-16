fca938d2685024a9fefb9b9ec36c965f876bd31e
thisthat587 <thisthat587@gmail.com>
2024-01-16 07:51:40 +0530

    .

--- Binary-Search/SplitArrayLargestSum.java
+++ Binary-Search/SplitArrayLargestSum.java
@@ -3,40 +3,40 @@
 // A subarray is a contiguous part of the array.
 
 public class SplitArrayLargestSum {
-    public static void main(String[] args) {
-        int arr[] = { 7, 2, 5, 10, 8 };
-        int k = 5;
-        System.out.println(splitArray(arr, k));
-    }
+      public static void main(String[] args) {
+            int arr[] = { 7, 2, 5, 10, 8 };
+            int k = 5;
+            System.out.println(splitArray(arr, k));
+      }
 
-    static int splitArray(int[] nums, int k) {
-        int start = 0;
-        int end = 0;
-        int mid = 0;
-        for (int i = 0; i < nums.length; i++) {
-            start = Math.max(start, nums[i]);
-        }
-        for (int i = 0; i < nums.length; i++) {
-            end = end + nums[i];
-        }
-        while (start < end) {
-            int pieces = 1;
-            int currentSum = 0;
-            mid = start + (end - start) / 2;
-            for (int num : nums) {
-                if (currentSum + num > mid) {
-                    pieces++;
-                    currentSum = num;
-                } else {
-                    currentSum += num;
-                }
+      static int splitArray(int[] nums, int k) {
+            int start = 0;
+            int end = 0;
+            int mid = 0;
+            for (int i = 0; i < nums.length; i++) {
+                  start = Math.max(start, nums[i]);
             }
-            if (pieces <= k) {
-                end = mid;
-            } else {
-                start = mid + 1;
+            for (int i = 0; i < nums.length; i++) {
+                  end = end + nums[i];
             }
-        }
-        return start;
-    }
+            while (start < end) {
+                  int pieces = 1;
+                  int currentSum = 0;
+                  mid = start + (end - start) / 2;
+                  for (int num : nums) {
+                        if (currentSum + num > mid) {
+                              pieces++;
+                              currentSum = num;
+                        } else {
+                              currentSum += num;
+                        }
+                  }
+                  if (pieces <= k) {
+                        end = mid;
+                  } else {
+                        start = mid + 1;
+                  }
+            }
+            return start;
+      }
 }
---
+++ Binary-Search/StudyPlan.java
@@ -0,0 +1,5 @@
+public class StudyPlan {
+    public static void main(String[] args){
+
+    }
+}
