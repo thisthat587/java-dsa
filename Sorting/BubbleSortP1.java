2b3e86d06cbf2b680cbc53594f1dfdf1f14c919a
thisthat587 <thisthat587@gmail.com>
2024-01-17 14:24:40 +0530

    startof sorting algo

--- Sorting/BubbleSortP1.java
+++ Sorting/BubbleSortP1.java
@@ -1,5 +1,19 @@
 package Sorting;
 
 public class BubbleSortP1 {
-    
+    public static void main(String[] args) {
+
+        int[] arr = { 3, 54, 5, 6, 43, 3, 2, 5, 6, 6, 4, 3, 2, 1 };
+        sortArr(arr);
+    }
+
+    static int[] sortArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+
+            }
+        }
+
+        return arr;
+    }
 }
