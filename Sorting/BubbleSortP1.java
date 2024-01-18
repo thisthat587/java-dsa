1f294b1fbcfd8aba4ddd78ece6134bed553b2dd6
thisthat587 <thisthat587@gmail.com>
2024-01-18 11:56:41 +0530

    .

--- Sorting/BubbleSortP1.java
+++ Sorting/BubbleSortP1.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class BubbleSortP1 {
 
   public static void main(String[] args) {
-    int[] arr = { 333, 54, 5, 6, 43, 3, 2, 5, 6, 6, 4, 3, 2, 1 };
+    // int[] arr = { 333, 54, 5, 6, 43, 3, 2, 5, 6, 6, 4, 3, 2, 1 };
     int[] brr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
     System.out.println(Arrays.toString(sortArr(brr)));
   }
